4a271262fcec5215c87925ffcbd28162c5a26959
Filipe Maia <filipe.c.maia@gmail.com>
2017-05-12 18:06:38 +0200

    Make docstrings conform to numpydoc

--- fib.py
+++ fib.py
@@ -1,5 +1,24 @@
 def fib(n):
-    """Return the first Fibonacci number above n."""
+    """
+    Return the first Fibonacci number above n.
+
+    Iteratively calculate Fibonacci numbers until it finds one
+    greater than n, which it then returns.
+
+    Parameters
+    ----------
+    n : integer
+      The minimum threshold for the desired Fibonacci number.    
+    output : integer
+      The first Fibonacci number greater than the input, `n`.
+
+    Examples
+    --------
+    >>> fib.fib(1)
+    2
+    >>> fib.fib(3)
+    5
+    """
     a = 0
     b = 1
     while b <= n:
